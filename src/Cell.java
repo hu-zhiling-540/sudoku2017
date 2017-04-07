666d7271a9f8849b9529df1ac64eb04b6d358918
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-04-06 23:53:20 -0400

    Put back implemented methods due to a data loss

--- src/Cell.java
+++ src/Cell.java
@@ -1,61 +1,79 @@
 import java.util.ArrayList;
 
 public class Cell {
-	
-	public int col;		// col coordinate
+
 	public int row;		// row coordinate
+	public int col;		// col coordinate
 	public Integer val;		// cell value
 	public ArrayList<Integer> domain;
-	
+
 
 	/**
-	 * Constructor
+	 * Default Constructor
+	 * @param row
+	 * @param col
 	 */
-	public Cell(int col, int row)	{
+	public Cell(int row, int col)	{
 		this.col = col;
 		this.row = row;
 		val = 0;
 		initDomain();
 	}
-	
-	public Cell(int col, int row, Integer val)	{
-		this.col = col;
+
+
+	/**
+	 * Default Constructor that takes in a specific value
+	 * @param row
+	 * @param col
+	 * @param val
+	 */
+	public Cell(int row, int col, Integer val)	{
 		this.row = row;
+		this.col = col;
 		this.val = val;
 		initDomain();
 	}
-	
-	
+
+
 	/**
 	 * Initialize the domain, 1 to 9 included
 	 */
 	public void initDomain()	{
 		this.domain = new ArrayList<Integer>();
 		for (int i = 1; i <= 9; i++)
-			this.domain.add(i);
+			this.domain.add(Integer.valueOf(i));
 	}
-	
-	
+
+
 	/**
-	 * Setter for the value variable
+	 * Setter for the value
 	 * @param val
 	 */
 	public void setValue(int val)	{
+		this.val = Integer.valueOf(val);
+	}
+
+
+	/**
+	 * Setter for the value
+	 * @param val
+	 */
+	public void setValue(Integer val)	{
 		this.val = val;
 	}
-	
-	
+
+
 	/**
-	 * Getter for the domain variable
+	 * Getter for the domain
 	 * @return
 	 */
 	public ArrayList<Integer> getDomain()	{
 		return this.domain;
 	}
-	
-	
+
+
 	/**
-	 * Setter for the domain variable
+	 * Setter for the domain
 	 * @param domain
 	 */
 	public void setDomain(ArrayList<Integer> domain)	{
--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -7,130 +7,204 @@ import java.util.Scanner;
 
 
 public class SudokuSolver {
-	
+
 	protected final static int ROWS = 9;
 	protected final static int COLS = 9;
-	
-	protected static Cell[][] grid;	// two dimensional grid
-	
+
+	protected static Cell[][] grid;	// two dimensional grid: R, C
+
 	File inputFile;
-	
+
 	// keep track
-	static long startTime;
-	static long endTime;
-	int nodesVisited = 0;		// number of nodes visited ( the number of variables assigned)
-	
+	static int nodesVisited = 0;		// number of nodes visited ( the number of variables assigned)
+	static int numBacktracks = 0;
+
 	protected ArrayList<Cell> freeVars;
 
-	
+
 	/**
 	 * constructor
 	 * @param filename
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public SudokuSolver(File file) throws IOException	{
+
 		inputFile = file;
 		grid = new Cell[ROWS][COLS];	// Instantiate the grid
 		readFile(inputFile);
 		printGrid();
-		
-		// naive backtracking
 	}
-	
+
 
 	/**
-	 * 
+	 * Reads a file and creates a two-dimensional grid based on the input values
 	 * @param file
 	 * @throws IOException
 	 * Ref.:https://docs.oracle.com/javase/tutorial/essential/io/scanning.html
 	 */
 	protected void readFile(File file) throws IOException	{
-		
+
 		Scanner s = null;
+
 		try {
-            s = new Scanner(new BufferedReader(new FileReader(file)));
-            
-            for (int i = 0; i < ROWS; i++)		{
-            	for (int j = 0 ; j < COLS; j++)		{
-            		if (s.hasNext()) {
-                    	String str = s.next(); 
-                    	grid[i][j] = new Cell(i, j, Integer.parseInt(str));
-//                      char[] myChar = str.toCharArray();
-                    }
-            	}
-            }
-            initLists();
-            
-        }
+			s = new Scanner(new BufferedReader(new FileReader(file)));
+
+			for (int i = 0; i < ROWS; i++)		{
+				for (int j = 0 ; j < COLS; j++)		{
+					if (s.hasNext()) {
+						String str = s.next();
+						grid[i][j] = new Cell(i, j, Integer.parseInt(str));
+					}
+				}
+			}
+		}
+
 		finally {
 			if (s != null) {
-                s.close();
-            }
-        }	
-	}
-	
-	protected void initLists()		{
-		
-		freeVars = new ArrayList<Cell>();
-		
-		for (int i = 0; i < ROWS; i++)		{
-        	for (int j = 0 ; j < COLS; j++)		{
-        		if (grid[i][j].val == 0)	{
-        			freeVars.add(grid[i][j]);
-        		}
-        	}
+				s.close();
+			}
 		}
-		
+	}
+
+
+	/**
+	 * Initializes a new ArrayList for free variables
+	 */
+	protected void initList()		{
+
+		freeVars = new ArrayList<Cell>();
+
+		for (int i = 0; i < ROWS; i++)		{
+			for (int j = 0 ; j < COLS; j++)		{
+				if (grid[i][j].val.equals(0))	{
+					freeVars.add(grid[i][j]);
+				}
+			}
+		}
+
 		System.out.println("Number of cells to be filled is " + freeVars.size() + " .");
 	}
-	
-	
+
+
+	/**
+	 * Selects a free variable and branches out on this node with value from its domain.
+	 * If a constraint check fails, the next domain value will be tried.
+	 */
+	protected void naiveBktrk()		{
+		// put all unassigned variables in a list
+		initList();
+		if (!freeVars.isEmpty())	{
+			Cell head = freeVars.remove(0);
+			naiveBacktracking(head);
+		}
+	}
+
+
+	/**
+	 * Recursive call on free variables until a solution is found
+	 * @param currCell
+	 * @return
+	 */
+	protected boolean naiveBacktracking(Cell currCell)	{
+
+		numBacktracks++;		// for tracking
+
+		// if no 0 entry in the grid
+		if (isSolved())
+			return true;
+
+		// loop through the domain
+		for (int i = 0; i < currCell.domain.size(); i++)	{
+
+			nodesVisited++;		// for tracking
+
+			Integer val = currCell.domain.get(i);
+
+			// check if no connected variables already assigned with the same value
+			if (!hasConflict(currCell, val))	{
+
+				grid[currCell.row][currCell.col].setValue(val);		// set temporary value
+
+				// based on this assignment: check the next variable
+				if (freeVars.isEmpty())
+					return true;			// by the time all the assignments are finished
+
+				// else: pop up a free variable from the list
+				Cell next = freeVars.get(0);
+				freeVars.remove(0);
+
+				if (naiveBacktracking(next))
+					return true;
+
+				// else: naive is wrong after, so we go back
+				grid[currCell.row][currCell.col].setValue(0);
+				freeVars.add(0, next);
+			}
+		}
+		return false;
+	}
+
+
 	/**
 	 * Backtracking with forward checking
 	 */
 	protected void bktrkFwdCkg()	{
-		// start with a cell that has the smallest domain
-		Cell toExamine = smallestDomain(freeVars);
-		backtracking(toExamine);
-		
+		// put all unassigned variables in a list
+		initList();
+		if (!freeVars.isEmpty())	{
+			Cell head = freeVars.get(0);
+			backtracking(head);
+		}
 	}
-	
-	
+
+
 	/**
 	 * Runs backtracking recursively
 	 * @param currCell
 	 * @return
 	 */
 	protected boolean backtracking(Cell currCell)	{
-		// base case
-		if (freeVars.size() == 0 && isSolved())
+
+		numBacktracks++;		// for tracking
+
+		// if no 0 entry in the grid, it is solved
+		if (isSolved())
 			return true;
-		
-		freeVars.remove(currCell);
+
+		freeVars.remove(0);
+
 		for (int i = 0; i < currCell.domain.size(); i++)	{
-			Integer val = currCell.domain.get(0);
-			if (forwardChecking(currCell, val))		{
-				Cell next = smallestDomain(freeVars);
-				if (backtracking(next))
-					return true;
+
+			nodesVisited++;		// for tracking
+
+			Integer val = currCell.domain.get(i);
+
+			// if connected cells don't contain the same value
+			if (!hasConflict(currCell, val))		{
+
+				grid[currCell.row][currCell.col].setValue(val);
+
+				// base case
+				if (freeVars.isEmpty())
+					return true;			// by the time all the assignments are finished
+
+				if (forwardChecking(currCell, val))		{
+					// else: pop up a free variable from the list
+					Cell next = freeVars.get(0);
+
+					if (backtracking(next))
+						return true;
+					else	{
+						grid[next.row][next.col].setValue(0);
+						freeVars.add(0, next);
+						// put value back to domains
+						undo(currCell,val);
+					}
+				}
 			}
+			// move on the next possible value in the domain
 		}
 		return false;
-	}
-	
-	
-	/**
-	 * If all cells are filled in with a value other than 0
-	 * @return
-	 */
-	protected boolean isSolved() {
-		for (int i = 0; i < ROWS; i++)		{
-        	for (int j = 0 ; j < COLS; j++)		{
-        		if (grid[i][j].val == 0)
-        			return false;
-        	}
-		}
-		return true;
 	}
 
 
@@ -142,32 +216,240 @@ public class SudokuSolver {
 	 * @return
 	 */
 	protected boolean forwardChecking(Cell currCell, Integer tempVal)	{
-		
+
 		// a list to keep track of connected cells, domain of which has been cleared
 		ArrayList<Cell> involved = new ArrayList<Cell>();
-		
+
 		for (int i = 0; i < freeVars.size(); i++)	{
-			if (checkConnected(currCell, freeVars.get(i)))	{
+			if (isConnected(currCell, freeVars.get(i)))		{
 				if (freeVars.get(i).domain.contains(tempVal))	{
 					freeVars.get(i).domain.remove(tempVal);
 					involved.add(freeVars.get(i));
 				}
 			}
 		}
-		
+
 		// as soon as the domain of any variable becomes empty, return to backtrack
-		if (!checkDomains(involved))	{
-			for (int j = 0; j < involved.size(); j++)	{
+		if (emptyDomain(involved))	{
+			for (int i = 0; i < involved.size(); i++)	{
 				// put value back to the domain
-				involved.get(j).domain.add(tempVal);
+				int index = findCellIndex(freeVars, involved.get(i));
+				if ( index != -1)
+					freeVars.get(index).domain.add(tempVal);
 			}
-			return false;	// not valid for the assigned value
+			return false;		// not valid for the assigned value
+		}
+
+		return true;
+	}
+
+
+	/**
+	 * Backtracking with forward checking
+	 */
+	protected void bktrkFwdCkg_MRV()	{
+		// put all unassigned variables in a list
+		initList();
+		if (!freeVars.isEmpty())	{
+			Cell head = smallestDomain(freeVars);
+			backtracking_MRV(head);
+		}
+	}
+
+
+	/**
+	 * Runs backtracking recursively
+	 * @param currCell
+	 * @return
+	 */
+	protected boolean backtracking_MRV(Cell currCell)	{
+
+		numBacktracks++;		// for tracking
+
+		// if no 0 entry in the grid, it is solved
+		if (isSolved())
+			return true;
+
+		freeVars.remove(currCell);
+
+		for (int i = 0; i < currCell.domain.size(); i++)	{
+
+			nodesVisited++;		// for tracking
+
+			Integer val = currCell.domain.get(i);
+
+			// if connected cells don't contain the same value
+			if (!hasConflict(currCell, val))		{
+
+				grid[currCell.row][currCell.col].setValue(val);
+
+				// base case
+				if (freeVars.isEmpty())
+					return true;			// by the time all the assignments are finished
+
+				if (forwardChecking(currCell, val))		{
+					// else: pop up a free variable from the list
+					Cell next = smallestDomain(freeVars);
+
+					if (backtracking_MRV(next))
+						return true;
+					else	{
+						grid[next.row][next.col].setValue(0);
+						freeVars.add(0, next);
+						// put value back to domains
+						undo(currCell,val);
+					}
+				}
+			}
+			// move on the next possible value in the domain
+		}
+		return false;
+	}
+
+
+	/**
+	 * Helper method to put value back to domains
+	 * @param cell
+	 * @param val
+	 */
+	protected void undo(Cell cell, Integer val)	{
+		for (int i = 0; i < freeVars.size(); i++)	{
+			if( isConnected(cell, freeVars.get(i)))	{
+				freeVars.get(i).domain.add(val);
+			}
+		}
+	}
+
+
+	/**
+	 * Helper method to check if newly assigned value
+	 * doesn't conflict with other connected cells
+	 * @param cell
+	 * @param val
+	 * @return
+	 */
+	protected boolean hasConflict(Cell cell, Integer val)	{
+
+		// same col
+		for (int i = 0; i < ROWS; i++)	{
+			if (grid[i][cell.col].val.equals(val))	{
+				return true;
+			}
+		}
+
+		// same row
+		for (int j = 0; j < COLS; j++)	{
+			if (grid[cell.row][j].val.equals(val))
+				return true;
+		}
+
+		// same block
+		int block_r = (cell.row/3)*3;
+		int block_c = (cell.col/3)*3;
+		for (int i = 0; i < 3 ; i++)	{
+			for (int j = 0; j < 3; j++)
+				if ( grid[i+block_r][j+block_c].val.equals(val))
+					return true;
+		}
+
+		return false;
+	}
+
+
+	/**
+	 * Helper method to find a certain index in the passed-in list
+	 * for a certain cell
+	 * @param list
+	 * @param cell
+	 * @return
+	 */
+	protected int findCellIndex(ArrayList<Cell> list, Cell cell)	{
+		for (int i = 0; i < list.size(); i++ )	{
+			if (list.get(i).row == cell.row && list.get(i).col == cell.col)
+				return i;
+		}
+		return -1;
+	}
+
+
+	/**
+	 * Helper method to check if two cells passed in are connected
+	 * @param main
+	 * @param cell
+	 * @return
+	 */
+	protected boolean isConnected(Cell main, Cell cell)	{
+
+		// main cell
+		int r1 = main.row;
+		int c1 = main.col;
+
+		// cell to be checked
+		int r2 = cell.row;
+		int c2 = cell.col;
+
+		// same row or same col
+		if (c1 == c2|| r1 == r2)
+			return true;
+
+		// same block
+		for (int i = (r1/3)*3; i < (r1/3)*3 + 3; i++)	{
+			for (int j = (c1/3)*3; j < (c1/3)*3 + 3; j++)
+				if ( i == r2 && j == c2 && !( r1 == r2 && c1 == c2 ))		// don't count self in
+					return true;
+		}
+
+		return false;
+
+	}
+
+
+	/**
+	 * If all cells are filled in with a value other than 0
+	 * @return
+	 */
+	protected boolean isSolved() {
+		for (int i = 0; i < ROWS; i++)		{
+			for (int j = 0 ; j < COLS; j++)		{
+				if (grid[i][j].val.equals(0))
+					return false;
+			}
 		}
 		return true;
-		
 	}
-	
-	
+
+
+	/**
+	 * Helper method to check if at least one cell has an empty domain,
+	 * which means it has no value to be assigned to,
+	 * returns false
+	 * @return
+	 */
+	protected boolean emptyDomain(ArrayList<Cell> list)	{
+		for (int i = 0; i < list.size(); i++)	{
+			if (list.get(i).domain.isEmpty())
+				return true;
+		}
+		return false;
+	}
+
+
+	/**
+	 * Chooses the best variable to assign next
+	 * based on MRV heuristic, the fewest remaining assignable values
+	 * @param list
+	 * @return
+	 */
+	protected Cell smallestDomain(ArrayList<Cell> list)		{
+		Cell temp = list.get(0);
+		for (int i = 1; i < list.size(); i++)	{
+			if (list.get(i).domain.size() < temp.domain.size())
+				temp = list.get(i);
+		}
+		return temp;
+	}
+
+
 	/**
 	 * Finds the least-constraining-value,
 	 * which rules out the fewest choices for the connected variables
@@ -180,92 +462,93 @@ public class SudokuSolver {
 	}
 
 
-//	minimum remaining value (MRV) heuristic. 
-//	choosing the best variable to assign next, 
-//	based on the size of its domain. More specifically, 
-//	we choose the cell having the fewest remaining assignable values.
-	
-	protected Cell smallestDomain(ArrayList<Cell> list)		{
-		Cell temp = list.get(0);
-		for (int i = 1; i < list.size(); i++)	{
-			if (list.get(i).domain.size() < temp.domain.size())
-				temp = list.get(i);
-		}
-		return temp;
-	}
-	/**
-	 * Checks domains of free cells
-	 * if at least one cell has an empty domain,
-	 * which means it has no value to be assigned to,
-	 * returns false
-	 * @return
-	 */
-	protected boolean checkDomains(ArrayList<Cell> list)	{
-		for (int i = 0; i < list.size(); i++)	{
-			if (list.get(i).domain.isEmpty())
-				return false;
-		}
-		return true;
-	}
-	
-	/**
-	 * Checks two cells passed in if they are connected
-	 * @param main
-	 * @param cell
-	 * @return
-	 */
-	protected boolean checkConnected(Cell main, Cell cell)	{
-		
-		// main cell
-		int c1 = main.col;
-		int r1 = main.row;
-		
-		// cell to be checked
-		int c2 = cell.col;
-		int r2 = cell.row;
-		
-		
-		// same block
-		for (int i = (c1/3)*3; i < (c1/3)*3 + 3; i++)	{
-			for (int j = (r1/3)*3; j < (r1/3)*3 + 3; j++)
-				if ( i == c2 && j == r2)
-					return true;
-		}
-		
-		// same row or same col
-		if (c1 == c2|| r1 == r2)
-			return true;
-		
-		// else
-		return false;
-		
-	}
-	
-	
 	/**
 	 * Prints the grid out to the console
 	 */
 	protected void printGrid() {
 		String output = "";
 		for (int i = 0; i < ROWS; i++)		{
-	    	for (int j = 0 ; j < COLS; j++)		{
-				output += grid[i][j].val + " ";
+			for (int j = 0 ; j < COLS; j++)		{
+				if ( j%3 == 0 && j != 0)
+					output += "| ";
+				output +=grid[i][j].val + " ";
 			}
+
+			if ( i == 2 || i == 5)	{
+				output += "\n";
+				output += "- - -   - - -   - - - ";
+			}
+
 			output += "\n";
 		}
 		System.out.println(output);
 	}
 
 
+
+
 	/**
 	 * Main method that takes arguments from command line
 	 */
-	protected static void main(String[] args) throws IOException	{
-		
+	public static void main(String[] args) throws IOException	{
+
 		File file = new File(args[0]);
-		SudokuSolver sudoku = new SudokuSolver(file);
-        
-	}  
-	
+
+		if (args.length == 1)	{
+			System.out.println("Naive Backtracking");
+			long program_start = System.nanoTime();
+			SudokuSolver sudoku = new SudokuSolver(file);
+			long search_start =  System.nanoTime();
+			sudoku.naiveBktrk();
+			long total_end = System.nanoTime();
+			sudoku.printGrid();
+			System.out.println("TOTAL_TIME: " + ((total_end - program_start)/1000000000.0));
+			System.out.println("SEARCH_TIME: " + ((total_end - search_start)/1000000000.0));
+			System.out.println("NODES_VISITED: " + nodesVisited);
+			System.out.println("NUM_BACKTRACKS: " + numBacktracks);
+
+
+		}
+
+		else if (args.length == 2){
+			if(args[1].equals("FC")){
+				System.out.println("Backtracking with Forward Checking");
+				long program_start = System.nanoTime();
+				SudokuSolver sudoku = new SudokuSolver(file);
+				long search_start =  System.nanoTime();
+				sudoku.bktrkFwdCkg();
+				long total_end = System.nanoTime();
+				sudoku.printGrid();
+				System.out.println("TOTAL_TIME: " + ((total_end - program_start)/1000000000.0));
+				System.out.println("SEARCH_TIME: " + ((total_end - search_start)/1000000000.0));
+				System.out.println("NODES_VISITED: " + nodesVisited);
+				System.out.println("NUM_BACKTRACKS: " + numBacktracks);
+			}
+			else
+				System.out.println("Not legal arguments");
+		}
+
+		else if (args.length == 3)
+			if(args[1].equals("FC")&& args[2].equals("MRV")
+					|| args[2].equals("FC")&& args[1].equals("MRV")){
+				System.out.println("Backtracking with Forward Checking with MRV heuristic");
+				long program_start = System.nanoTime();
+				SudokuSolver sudoku = new SudokuSolver(file);
+				long search_start =  System.nanoTime();
+				sudoku.bktrkFwdCkg_MRV();
+				long total_end = System.nanoTime();
+				sudoku.printGrid();
+				System.out.println("TOTAL_TIME: " + ((total_end - program_start)/1000000000.0));
+				System.out.println("SEARCH_TIME: " + ((total_end - search_start)/1000000000.0));
+				System.out.println("NODES_VISITED: " + nodesVisited);
+				System.out.println("NUM_BACKTRACKS: " + numBacktracks);
+			}
+			else
+				System.out.println("Not legal arguments");
+		else
+			System.out.println("Not legal arguments");
+	}
+
+
 }
 
